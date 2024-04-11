ca6a0192f095e757d09e0247e1f0c2300ed1e59e
Guilherme <guilhermersouza34@gmail.com>
2024-04-11 14:35:02 -0300

    Adicionando Js ao Projeto

---
+++ main.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function(e) {
+    const nameElement = document.querySelector('#name');
+    const usernameElement = document.querySelector('#username');
+    const avatarElement = document.querySelector('#avatar');
+    const reposElement = document.querySelector('#repos');
+    const followersElement = document.querySelector('#followers');
+    const followingElement = document.querySelector('#following');
+    const linkElement = document.querySelector('#link');
+});
